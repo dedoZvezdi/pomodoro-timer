85ef6e99d83ccc26dd6d7137e93efab3e5b89aa0
Boyan <72163457+boce1@users.noreply.github.com>
2025-06-17 17:19:53 +0300

    remove entry place spinbox

--- config_ui/__init__.py
+++ config_ui/__init__.py
@@ -32,22 +32,25 @@ class Config_window:
         hour_label = tk.Label(self.root, text="Hour:")
         hour_label.grid(column=0, row=0, sticky=tk.EW, padx=5, pady=5)
 
-        hour_entry = tk.Entry(self.root)
-        hour_entry.grid(column=1, row=0, sticky=tk.EW, padx=5, pady=5)
+        hour_current_value = tk.StringVar(value=0)
+        hour_spin_box = tk.Spinbox(self.root, from_=0, to=24, textvariable=hour_current_value)
+        hour_spin_box.grid(column=1, row=0, sticky=tk.EW, padx=5, pady=5)
 
         # minutes
         minute_label = tk.Label(self.root, text="Minute:")
         minute_label.grid(column=0, row=1, sticky=tk.EW, padx=5, pady=5)
 
-        minute_entry = tk.Entry(self.root,  show="*")
-        minute_entry.grid(column=1, row=1, sticky=tk.EW, padx=5, pady=5)
+        minute_current_value = tk.StringVar(value=0)
+        minute_spin_box = tk.Spinbox(self.root, from_=0, to=60, textvariable=minute_current_value)
+        minute_spin_box.grid(column=1, row=1, sticky=tk.EW, padx=5, pady=5)
 
         # seconds
         minute_label = tk.Label(self.root, text="Seconds:")
         minute_label.grid(column=0, row=2, sticky=tk.EW, padx=5, pady=5)
 
-        minute_entry = tk.Entry(self.root,  show="*")
-        minute_entry.grid(column=1, row=2, sticky=tk.EW, padx=5, pady=5)
+        second_current_value = tk.StringVar(value=0)
+        second_spin_box = tk.Spinbox(self.root, from_=0, to=60, textvariable=second_current_value)
+        second_spin_box.grid(column=1, row=2, sticky=tk.EW, padx=5, pady=5)
 
         update_button = tk.Button(self.root, text="Update time")
         update_button.grid(column=0, row=3, padx=5, pady=5, columnspan=2)
